950241d5070cf0644d7e10fb97a3ee5402502371
baumgartner <baumgartner@10eb3eed-6697-4d8c-9aac-32ebf1d36239>
2013-08-03 18:12:51 +0000

    bugfix
    git-svn-id: https://src.ifi.uzh.ch/ailab/esmcs/src/python/lib/rrl@1444 10eb3eed-6697-4d8c-9aac-32ebf1d36239

--- rrl/analysis_puppy.py
+++ rrl/analysis_puppy.py
@@ -4,5 +4,5 @@ Analysis tools, specific for the *Puppy* experiment.
 
 import pylab
 
-def plot_epuck_snapshot(axis, critic, trajectory, sample_actions, init_steps=1, traj_chosen=None):
-    pass
+#def plot_epuck_snapshot(axis, critic, trajectory, sample_actions, init_steps=1, traj_chosen=None):
+#    pass
