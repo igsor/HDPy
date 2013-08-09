2690f11af034d02838dc59aa890f2ea527c2c8a0
baumgartner <baumgartner@10eb3eed-6697-4d8c-9aac-32ebf1d36239>
2013-08-09 13:29:04 +0000

    Puppy Analysis
    git-svn-id: https://src.ifi.uzh.ch/ailab/esmcs/src/python/lib/rrl@1458 10eb3eed-6697-4d8c-9aac-32ebf1d36239

--- rrl/analysis_puppy.py
+++ rrl/analysis_puppy.py
@@ -3,26 +3,55 @@ Analysis tools, specific for the *Puppy* experiment.
 """
 
 import pylab
+import numpy as np
 
-def puppy_plot_trajectory(analysis, axis, episode, **kwargs):
+def puppy_plot_trajectory(analysis, axis, episode, step_width=1, **kwargs):
     """Plot the trajectory of an episode
     """
-    gps_x = analysis[episode]['puppyGPS_x'][149::150]
-    gps_y = analysis[episode]['puppyGPS_y'][149::150]
+    gps_x = analysis[episode]['puppyGPS_x'][step_width-1::step_width]
+    gps_y = analysis[episode]['puppyGPS_y'][step_width-1::step_width]
+    if step_width > 1:
+        gps_x = np.concatenate(([analysis[episode]['puppyGPS_x'][0]], gps_x))
+        gps_y = np.concatenate(([analysis[episode]['puppyGPS_y'][0]], gps_y))
+
     col = kwargs.pop('color', 'k')
-    axis.plot(gps_x, gps_y, '*', color=col, **kwargs)
+    label = kwargs.pop('label', 'Trajectory')
     axis.plot(gps_x, gps_y, color=col, **kwargs)
+    axis.plot(gps_x[0], gps_y[0], 'ks', label='Start')
+    axis.plot(gps_x[-1], gps_y[-1], 'k^', label='End')
     return axis
 
-def puppy_plot_all_trajectories(analysis, axis):
+def puppy_plot_all_trajectories(analysis, axis, step_width=1, **kwargs):
     """Plot all trajectories in ``analysis`` into ``axis``.
     """
     gps_x = analysis.get_data('puppyGPS_x')
     gps_y = analysis.get_data('puppyGPS_y')
     
     N = len(gps_x)-1
-    for x, y in zip(gps_x, gps_y):
+    kwargs.pop('color', None) # remove color argument
+    for idx, (x, y) in enumerate(zip(gps_x, gps_y)):
         col = 0.75 - (0.75 * (idx - 1))/N
-        axis.plot(x, y, color=col)
+        
+        x_plot = np.concatenate(([x[0]], x[step_width-1::step_width]))
+        y_plot = np.concatenate(([y[0]], y[step_width-1::step_width]))
+        
+        axis.plot(x_plot, y_plot, color=str(col), **kwargs)
     
-    return ax
+    return axis
+
+def puppy_plot_linetarget(axis, origin=(2.0, 0.0), direction=(1.0, 1.0), range_=(-5.0, 5.0)):
+    """Plot a line given by ``origin`` and ``direction``. The ``range_``
+    may be supplid, which corresponds to the length of the line (from
+    the origin).
+    """
+    origin = np.array(origin)
+    dir_ = np.array(direction)
+    dir_ /= np.linalg.norm(dir_)
+    line = [origin + t * dir_ for t in range_]
+    line_x, line_y = zip(*line)
+    axis.plot(line_x, line_y, 'k', label='Target')
+    return axis
+
+def puppy_plot_locationtarget():
+    pass
+
