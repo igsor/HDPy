1e4f9666a5f004d5d372dbb40c861390ff8a1c79
baumgartner <baumgartner@10eb3eed-6697-4d8c-9aac-32ebf1d36239>
2013-09-07 21:24:26 +0000

    videos :)
    git-svn-id: https://src.ifi.uzh.ch/ailab/esmcs/src/python/lib/rrl@1542 10eb3eed-6697-4d8c-9aac-32ebf1d36239

--- rrl/analysis_puppy.py
+++ rrl/analysis_puppy.py
@@ -217,29 +217,16 @@ def puppy_plot_action(analysis, episode, critic, reservoir, inspect_epochs, acti
         epoch_actions = [None] * len(inspect_epochs)
     
     for trg_epoch, actions in zip(inspect_epochs, epoch_actions):
-        reservoir.reset()
-        reservoir.states = np.atleast_2d(grp['x_curr'][trg_epoch-1,:])
-
-        # evaluate actions
-        # Note: epoch is one step ahead (of a_curr, same time as a_next)!
-        # Note: sensor values are shifted w.r.t a_curr by obs_offset
-        s_curr = dict([(sensor,grp[sensor][obs_offset+step_width*(trg_epoch-1):obs_offset+trg_epoch*step_width]) for sensor in sensor_names])
-        a_ret = np.zeros((len(actions_range_x), len(actions_range_y)))
-
-        actions_iter = itertools.product(range(len(actions_range_x)), range(len(actions_range_y)))
-        for idx_x, idx_y in actions_iter:
-            action_candidate = np.atleast_2d((actions_range_x[idx_x], actions_range_y[idx_y])).T
-            j_curr = critic(s_curr, action_candidate, simulate=True)
-            a_ret[idx_x, idx_y] = j_curr[0, 0]
-            #print actions_range_x[idx_x], actions_range_y[idx_y], j_curr[0, 0]
-
+        
+        # simulate the actions
+        a_ret = _puppy_action_eval(grp, reservoir, critic, trg_epoch, obs_offset, step_width, actions_range_x, actions_range_y)
+        
         # plot results
         fig = pylab.figure()
-        pylab.gray()
         # In the image, the y-axis is the rows, the x-axis the columns of the matrix
         # Having index (0,0) in the left/bottom corner: origin='lower'
         pylab.plot((0, len(actions_range_x)-1), (0, len(actions_range_y)-1), 'b')
-        pylab.imshow(a_ret, origin='lower')
+        pylab.imshow(a_ret, origin='lower', cmap=pylab.cm.gray)
         pylab.colorbar()
         pylab.xticks(range(len(actions_range_y)), actions_range_y)
         pylab.yticks(range(len(actions_range_x)), actions_range_x)
@@ -255,6 +242,80 @@ def puppy_plot_action(analysis, episode, critic, reservoir, inspect_epochs, acti
     return fig
 
 def puppy_plot_inspected_trajectory(analysis, episode_idx, step_width, axis, inspect_epochs, obs_offset):
+    puppy_plot_trajectory(analysis, axis, episode_idx, step_width, color='b')
+    trg_x = [analysis[episode_idx]['puppyGPS_x'][obs_offset + step_width*trg_epoch+step_width-1] for trg_epoch in inspect_epochs]
+    trg_y = [analysis[episode_idx]['puppyGPS_y'][obs_offset + step_width*trg_epoch+step_width-1] for trg_epoch in inspect_epochs]
+    axis.plot(trg_x, trg_y, 'k*', label='Inspected states')
+    return axis
+
+def _puppy_action_eval(grp, reservoir, critic, trg_epoch, obs_offset, step_width, actions_range_x, actions_range_y):
+    reservoir.reset()
+    reservoir.states = np.atleast_2d(grp['x_curr'][trg_epoch-1,:])
+    
+    # evaluate actions
+    # Note: epoch is one step ahead (of a_curr, same time as a_next)!
+    # Note: sensor values are shifted w.r.t a_curr by obs_offset
+    s_curr = dict([(sensor,grp[sensor][obs_offset+step_width*(trg_epoch-1):obs_offset+trg_epoch*step_width]) for sensor in sensor_names])
+    a_ret = np.zeros((len(actions_range_x), len(actions_range_y)))
+    
+    actions_iter = itertools.product(range(len(actions_range_x)), range(len(actions_range_y)))
+    for idx_x, idx_y in actions_iter:
+        action_candidate = np.atleast_2d((actions_range_x[idx_x], actions_range_y[idx_y])).T
+        j_curr = critic(s_curr, action_candidate, simulate=True)
+        a_ret[idx_x, idx_y] = j_curr[0, 0]
+        #print actions_range_x[idx_x], actions_range_y[idx_y], j_curr[0, 0]
+    
+    return a_ret
+
+def puppy_vid_action(image, (a_line, a_marker, px, py), grp, critic, reservoir, epoch, actions_range_x, actions_range_y, step_width, obs_offset, actions=None, actions_keep=False):
+    """
+    
+    .. todo::
+        documentation,
+    
+    """
+    a_ret = _puppy_action_eval(grp, reservoir, critic, epoch, obs_offset, step_width, actions_range_x, actions_range_y)
+    
+    # update plot
+    image.set_data(a_ret)
+    image.set_clim(vmin=a_ret.min(), vmax=a_ret.max())
+    image.changed()
+    
+    if actions is not None:
+        actions[:,0] = (actions[:,0] - py[0]) * py[1]
+        actions[:,1] = (actions[:,1] - px[0]) * px[1]
+        a_line.set_data((actions[:,1], actions[:,0]))
+        a_marker.set_data(([actions[-1,1]], [actions[-1,0]]))
+    
+    return image
+
+def puppy_vid_init(actions_range_x, actions_range_y, with_actions=True):
+    fig = pylab.figure()
+    axis = fig.add_subplot(111)
+    axis.set_xticks(range(len(actions_range_y)))
+    axis.set_xticklabels(actions_range_y)
+    axis.set_yticks(range(len(actions_range_x)))
+    axis.set_yticklabels(actions_range_x)
+    title = fig.suptitle('Expected Return per action')
+    axis.set_xlabel('Amplitude right legs') # cols are idx_y, right legs
+    axis.set_ylabel('Amplitude left legs') # rows are idx_x, left legs
+    axis.plot((0, len(actions_range_x)-1), (0, len(actions_range_y)-1), 'b')
+    img_data = np.zeros((len(actions_range_x), len(actions_range_y)))
+    axim = axis.imshow(img_data, origin='lower', cmap=pylab.cm.gray)
+    fig.colorbar(axim)
+    
+    # action line
+    if with_actions:
+        ox, sx = actions_range_x[0], len(actions_range_x)-1
+        oy, sy = actions_range_y[0], len(actions_range_y)-1
+        a_line = axis.plot([sx*(0.5-ox), sx*(0.5-ox)], [sy*(0.5-oy), sy*(1.0-oy)], 'r')[0]
+        a_marker = axis.plot([sx*(0.5-ox)], [sy*(0.5-oy)], 'rs')[0]
+    else:
+        ox = sx = oy = sy = a_line = a_marker = None
+    
+    return fig, axis, axim, title, (a_line, a_marker, (ox,sx), (oy,sy))
+
+def puppy_vid_inspected_trajectory(analysis, episode_idx, step_width, axis, inspect_epochs, obs_offset):
     puppy_plot_trajectory(analysis, axis, episode_idx, step_width, color='b')
     trg_x = [analysis[episode_idx]['puppyGPS_x'][obs_offset + step_width*trg_epoch+step_width-1] for trg_epoch in inspect_epochs]
     trg_y = [analysis[episode_idx]['puppyGPS_y'][obs_offset + step_width*trg_epoch+step_width-1] for trg_epoch in inspect_epochs]
